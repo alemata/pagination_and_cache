578efa7d0f89b20d3fa66e4184adbf32decd685a
Alejandro Mataloni <amataloni@gmail.com>
2019-02-12 12:51:51 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..40).each {|a| Property.create(name: a)}
